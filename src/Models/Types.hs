9315c68f1dd2071d76e5116c361b1acfc961c93c
arturcatao <arturcataodoo@gmail.com>
2026-06-01 14:08:27 -0300

    feat: new AppState and emptState Types

--- src/Models/Types.hs
+++ src/Models/Types.hs
@@ -11,3 +11,18 @@ data Priority
     | Medium
     | High
     deriving (Show, Eq, Ord, Enum)
+
+--toda vez que atualizar algo, um novo estado do programa tem que ser gerado
+data AppState = AppState
+  { users       :: [User]
+  , tasks       :: [Task]
+  , currentUser :: Maybe User
+  } deriving (Show)
+
+-- def do estado inicial/vazio
+emptyState :: AppState
+emptyState = AppState
+  { users       = []
+  , tasks       = []
+  , currentUser = Nothing
+  }
