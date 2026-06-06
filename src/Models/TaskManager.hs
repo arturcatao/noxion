e5880ea1a32bf0002cd81b177ab1d415f7a00119
malugomesp <malu.gomes439@gmail.com>
2026-06-06 15:47:17 -0300

    fix(TaskManager): corrige warnings de nome

--- src/Models/TaskManager.hs
+++ src/Models/TaskManager.hs
@@ -3,19 +3,19 @@ module Models.TaskManager where
 import Models.Task (Task(..), criarTask)
 import Models.AppState (AppState(..), tasks)
 import Models.Types (Status, Priority)
-import Models.User (userId)  
+import qualified Models.User as User  
 import Data.Time (Day)
 
 adicionarTask :: AppState -> String -> String -> Priority -> Maybe Day -> AppState
-adicionarTask estado titulo desc prioridade prazo = 
+adicionarTask estado novoTitulo novaDesc prioridade prazo =
     case currentUser estado of
     Nothing   -> estado
     -- primeiro caso: nao tem ngm logado, então devolve o estado sem modificar
     Just user ->
-        let uid = userId user
+        let uid = User.userId user
             novoId =  show (length (tasks estado) + 1)
             -- esse id é gerado baseado no tamanho da lista, mas podemos mudar a forma de fazer isso depois
-            novaTask =  criarTask novoId uid titulo desc prazo
+            novaTask = criarTask novoId uid novoTitulo novaDesc prioridade prazo
         in estado {tasks = tasks estado ++ [novaTask]}
 
 removerTask :: AppState -> String -> AppState
@@ -27,19 +27,19 @@ removerTask estado tid =
 alterarStatus:: AppState -> String -> Status -> AppState
 alterarStatus estado tid novoStatus =
     let listaAtualizada = map (\t -> 
-        if taskId t == tid 
-            then t {status = novoStatus} 
-            else t 
-        ) (tasks estado)
+            if taskId t == tid 
+                then t {status = novoStatus} 
+                else t 
+            ) (tasks estado)
     in estado {tasks = listaAtualizada}
 
 alterarPrioridade :: AppState -> String -> Priority -> AppState
 alterarPrioridade estado tid novaPrioridade =
     let listaAtualizada = map (\t -> 
-        if taskId t == tid 
-            then t {priority = novaPrioridade}
-            else t
-    ) (tasks estado)
+            if taskId t == tid 
+                then t {priority = novaPrioridade}
+                else t
+            ) (tasks estado)
     in estado {tasks = listaAtualizada}
 
 --  pequena observação: nunca mudem o estado, cada função recebe oum AppState 
